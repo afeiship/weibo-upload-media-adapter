c1ee150bd6fa0392fe93e76c3806d5f11a8919bf
feizheng <1290657123@qq.com>
2016-10-27 18:43:18 +0800

    bugfix

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var fs = require('fs');
 
-module.exports = function(inFilename, inDefault, inTab) {
-  var tab = inTab || 2;
-  var defaultJson = inDefault;
-  if (typeof inDefault == 'string') {
-    defaultJson = require(inDefault);
-  }
+exports function (inRequire) {
+  return function(inFilename, inDefault, inTab) {
+    var tab = inTab || 2;
+    var defaultJson = inDefault;
+    if (typeof inDefault == 'string') {
+      defaultJson = inRequire(inDefault);
+    }
 
-  fs.writeFileSync(
-    inFilename,
-    JSON.stringify(defaultJson, null, tab)
-  );
+    fs.writeFileSync(
+      inFilename,
+      JSON.stringify(defaultJson, null, tab)
+    );
+  };
 };
