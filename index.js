8d86d15a3d4def0d30cc9f8ebb0d2104e4cad86b
afeiship <1290657123@qq.com>
2021-12-18 23:30:51 +0800

    feat: initial

--- index.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-
-module.exports = function(inFilename, inDefaultJson, inTab) {
-  fs.writeFileSync(
-    inFilename,
-    JSON.stringify(inDefaultJson, null, inTab || 2)
-  );
-};
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import NxWeiboOss from '@jswork/next-weibo-oss';
+import NxLcOptions from '@afeiship/next-leancloud-options';
+
+const WEIBO_LC_ID = '60f768f6d9f1465d3b1d5c43';
+const fetch = window.fetch.bind(window);
+
+interface UploadEvent {
+  target: {
+    value: {
+      url: string;
+      file: File;
+    }[];
+  };
+}
+
+export default (inEvent: UploadEvent): Promise<string[]> => {
+  const lcOpts = new NxLcOptions({ fetch, id: WEIBO_LC_ID });
+  const lcRes = lcOpts.get();
+  const weiboOss = new NxWeiboOss(lcRes.value);
+  const files = inEvent.target.value.map((item) => item.file);
+  return new Promise((resolve) => {
+    weiboOss.uploads(files).then((res) => {
+      const urls = res.map((it) => it.url);
+      resolve(urls);
+    });
+  });
+};
